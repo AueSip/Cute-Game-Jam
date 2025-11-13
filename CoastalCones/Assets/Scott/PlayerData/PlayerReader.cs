0efbec0c8d7d72da5934dd32264ad52ac2dafc9f
AueSip <scottdeanfowler@gmail.com>
2025-11-13 14:29:48 +0000

    Update

---
+++ CoastalCones/Assets/Scott/PlayerData/PlayerReader.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    PlayerSaves playerSaveData;
+    void Start()
+    {
+        playerSaveData = JsonUtility.FromJson<PlayerSaves>(jsonFile.text);
+
+        foreach (PlayerSave data in playerSaveData.playerSaves)
+        {
+            Debug.Log("Found data: " + data.experience + " " + data.rating + " " + data.money + " " + data.currentDay);
+            foreach (string info in data.bought_machines)
+            {
+                Debug.Log("Found PlayerUnlocks:" + info);
+            }
+        }
+
+    }
+    public PlayerSave ReturnPlayerSave()
+    {
+        return playerSaveData.playerSaves[0];
+    }
+}
